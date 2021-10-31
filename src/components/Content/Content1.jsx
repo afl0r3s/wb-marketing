68cf4cdd70b897e9dd4506b431bd18e268f1e44a
Alejandro Flores <sjafs.83@gmail.com>
2021-10-30 22:50:59 -0300

    update content 2

--- src/components/Content/Content1.jsx
+++ src/components/Content/Content1.jsx
@@ -9,10 +9,10 @@ export default function Content1() {
             </div>
             <div className={style.content2}>
                 <h1>
-                    Why you should hire an agent who has partnered with a luxury real estate marketing company
+                    Why you should hire an agent who has partnered with a Property Company marketing
                 </h1>
                 <span>
-                    One of the biggest perks of working with a real estate agent is their ability to market your property to the right target audience. A professional agent with the right experience will be able to identify who will be interested in your property and implement effective strategies to reach them via their website, paid advertising, social media, and other means. When you work with an agent who has partnered with Luxury Presence, you benefit from the many different types of marketing capabilities the company offers. Here is how:
+                One of the biggest perks of working with us is their ability to market your property to the right target audience. A professional agent with the right experience will be able to identify who will be interested in your property and implement effective strategies to reach them via their website, paid advertising, social media, and other means. 
                 </span>
             </div>
         </section>
--- src/components/Content/Content2.jsx
+++ src/components/Content/Content2.jsx
@@ -9,7 +9,7 @@ export default function Content2() {
                     Paid Advertising on All Channels
                 </h1>
                 <span>
-                    To reach your potential buyers, you have to know who they are, where they spend their time online, and how they make their buying decisions. Agents who use luxury real estate marketing services are able to identify your niche market, then create paid ads on Facebook, Google, Instagram, and YouTube that reach those interested in your property. These paid ad campaigns are highly customized and targeted, ensuring you are not spending money marketing to those who are not in the market for a property such as yours
+                    To reach your potential buyers, you have to know who they are, where they spend their time online, and how they make their buying decisions.
                 </span>
             </div>
             <div className={style.content1_2}>
